0f3f98edb5f9d9fb031c2e4c38b388a6502d8819
Denny Ayard <djayard@ucdavis.edu>
2015-12-31 02:09:36 -0800

    First night's effort.

---
+++ Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour
+{
+    public float fadeSpeed = 2f;
+    public float highIntensity = 2f;
+    public float lowIntensity = 0.5f;
+    public float changeMargin = 0.2f;
+    public bool alarmOn;
+
+    private float targetIntensity;
+
+    void Awake()
+    {
+        GetComponent<Light>().intensity = 0f;
+        targetIntensity = highIntensity;
+
+    }
+
+    void Update()
+    {
+        if (alarmOn)
+        {
+            CheckTargetIntensity();
+            GetComponent<Light>().intensity = Mathf.Lerp(GetComponent<Light>().intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+            
+        }
+        else if( GetComponent<Light>().intensity > 0.01f )
+            GetComponent<Light>().intensity = Mathf.Lerp(GetComponent<Light>().intensity, 0f, fadeSpeed * Time.deltaTime);
+    }
+
+    void CheckTargetIntensity()
+    {
+        if( Mathf.Abs(GetComponent<Light>().intensity - targetIntensity) < changeMargin )
+        {
+            if (targetIntensity == highIntensity)
+                targetIntensity = lowIntensity;
+            else
+                targetIntensity = highIntensity;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/ScreenFadeInOut.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenFadeInOut : MonoBehaviour
+{
+    public float fadeSpeed = 1.5f;
+    private bool sceneStarting = true;
+
+    void Update()
+    {
+        if( sceneStarting )
+        {
+            StartScene();
+        }
+
+    }
+
+    void FadeToClear()
+    {
+        GetComponent<RawImage>().color = Color.Lerp(GetComponent<RawImage>().color, Color.clear, fadeSpeed * Time.deltaTime);
+    }
+
+    void FadeToBlack()
+    {
+        GetComponent<RawImage>().color = Color.Lerp(GetComponent<RawImage>().color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+
+    void StartScene()
+    {
+        FadeToClear();
+
+        if( GetComponent<RawImage>().color.a <= 0.05f)
+        {
+            GetComponent<RawImage>().color = Color.clear;
+            GetComponent<RawImage>().enabled = false;
+            sceneStarting = false;
+        }
+    }
+
+    public void EndScene()
+    {
+        GetComponent<RawImage>().enabled = true;
+        FadeToBlack();
+
+        if(GetComponent<RawImage>().color.a >= 0.95f)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
