7cc479e2bb256f8e493af0c2b77e7336c276aa50
Denny Ayard <djayard@ucdavis.edu>
2015-12-31 19:14:51 -0800

    Environment setup done.

---
+++ Assets/LaserSwitchDeactivation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserSwitchDeactivation : MonoBehaviour
+{
+    public GameObject laser;
+    public Material unlocked;
+    private GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+    }
+
+
+    void IsTriggerStay(Collider other)
+    {
+        if( other.gameObject == player )
+        {
+            if( Input.GetButton("Switch"))
+            {
+                LaserDeactivation();
+            }
+        }
+    }
+
+    void LaserDeactivation()
+    {
+        laser.SetActive(false);
+
+        Renderer screen = transform.Find("prop_switchUnit_screen_001").GetComponent<Renderer>();
+        screen.material = unlocked;
+        GetComponent<AudioSource>().Play();
+    }
+
+}
---
+++ Assets/Scripts/CCTVPlayerDetection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CCTVPlayerDetection : MonoBehaviour
+{
+    private GameObject player;
+    private LastPlayerSighting lastplayersighting;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        lastplayersighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if( other.gameObject == player )
+        {
+            Vector3 relPlayerpos = player.transform.position - GetComponent<Transform>().position;
+            RaycastHit hit;
+
+            if( Physics.Raycast(GetComponent<Transform>().position, relPlayerpos, out hit) )
+            {
+                if (hit.collider.gameObject == player)
+                    lastplayersighting.position = player.GetComponent<Transform>().position;
+            }
+        }
+
+    }
+}
